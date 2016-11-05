8c3b192f5859508e1524013dd89cc6eeed4d1831
Hodes <Hodes@Hodes-PC.nv.charter.com>
2016-11-05 16:04:10 -0700

    Added jUnit test sample

---
+++ src/jUnitTest/AgileTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jUnitTest;
+
+/**
+ *
+ * @author Hodes
+ */
+public class AgileTest {
+    public int add(String s1, String s2) {
+        // Function will be developed in the test. 
+        // convert string to int
+        int int1 = Integer.parseInt(s1);
+        int int2 = Integer.parseInt(s2);
+        return int1 + int2;
+    }
+    
+    public double addDec(String s3, String s4) {
+        // Function will be developed in the test. 
+        // convert string to int
+        double doub3 = Double.parseDouble(s3);
+        double doub4 = Double.parseDouble(s4);
+        return doub3 + doub4;
+    }
+}
---
+++ test/jUnitTest/AgileTestTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jUnitTest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Hodes
+ */
+public class AgileTestTest {
+    
+    public AgileTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class AgileTest.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        String s1 = "1";
+        String s2 = "3";
+        AgileTest instance = new AgileTest();
+        int expResult = 4;
+        int result = instance.add(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAdd2() {
+        System.out.println("add");
+        String s1 = "-1";
+        String s2 = "99";
+        AgileTest instance = new AgileTest();
+        int expResult = 98;
+        int result = instance.add(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAdd3() {
+        System.out.println("add");
+        String s1 = "1";
+        String s2 = "two";
+        AgileTest instance = new AgileTest();
+        int expResult = 3;
+        int result = instance.add(s1, s2);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of addDec method, of class AgileTest.
+     */
+    @Test
+    public void testAddDec() {
+        System.out.println("addDec");
+        String s3 = ".75";
+        String s4 = "1.75";
+        AgileTest instance = new AgileTest();
+        double expResult = 2.50;
+        double result = instance.addDec(s3, s4);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    @Test
+    public void testAddDec2() {
+        System.out.println("addDec");
+        String s3 = "5.5";
+        String s4 = "-1.75";
+        AgileTest instance = new AgileTest();
+        double expResult = 3.75;
+        double result = instance.addDec(s3, s4);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    @Test
+    public void testAddDec3() {
+        System.out.println("addDec");
+        String s3 = "Jeremy";
+        String s4 = "1.75";
+        AgileTest instance = new AgileTest();
+        double expResult = 3;
+        double result = instance.addDec(s3, s4);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+}
